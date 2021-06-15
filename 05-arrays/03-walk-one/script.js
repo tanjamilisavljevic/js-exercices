2933043e10700c53d2f5ed4ec8f93b0f1fe961cc
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-15 11:33:29 +0200

    Solve 05e03

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -27,5 +27,9 @@
     ];
 
     // your code here
-
+    document.getElementById('run').onclick = () => {
+        for (let i = 0; i < fruits.length; i++){
+            console.log(fruits[i]);
+        }
+    }
 })();
