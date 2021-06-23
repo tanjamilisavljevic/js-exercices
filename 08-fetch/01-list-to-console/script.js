3e2ef681f2643057dff92b09048133a101c9345a
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-23 11:15:58 +0200

    Solve 08e01

---
+++ 08-fetch/01-list-to-console/script.js
@@ -0,0 +1,16 @@
+/* becode/javascript
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    document.getElementById("run").addEventListener("click", function() {
+        fetch('api.json')
+        .then(response => response.json())
+            .then(data => console.log(data));
+    })
+})();
---
+++ 08-fetch/04-add-Optional/script.js
@@ -0,0 +1,12 @@
+/* becode/javascript
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+})();
