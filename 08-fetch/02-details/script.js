cd2607f9d41f652c30aa16f4c7ea6bd7de892c29
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-23 14:22:15 +0200

    Solve 08e02

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -7,6 +7,42 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
+
 (() => {
-    // your code here
+
+
+    document.getElementById("run").addEventListener("click", function () {
+        fetch('api.json')
+            .then(
+                function (response) {
+                    return response.json();
+                }
+            )
+            .then(
+                function (json) {
+                    const heroesList = json.heroes
+                    const heroId = parseInt(document.getElementById('hero-id').value);
+                    let hero = heroesList.find(function (hero){
+                        return hero.id === heroId;
+                    })
+
+                    const template = document.getElementById("tpl-hero");
+                    const clone = template.content.cloneNode(true);
+
+                    clone.querySelector('.name').textContent = hero.name;
+                    clone.querySelector('.alter-ego').textContent = hero.alterEgo;
+                    clone.querySelector('.powers').textContent = hero.abilities;
+
+                    document.getElementById('target').appendChild(clone);
+
+                }
+            )
+    })
+
+
+
+
+
 })();
