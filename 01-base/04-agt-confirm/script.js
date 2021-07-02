cf3d01cfeff9a4ab38f30dd42ab4e3a0ca96d076
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-02 10:45:02 +0200

    Rewrite script 01e04

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -9,20 +9,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+function fillInfo() {
 
-    // your code here
-    function fillInfo() {
-        let age = prompt("How old are you?");
-        let gender = prompt("How do you identify?");
-        let city = prompt("Where do you live?");
-        let newLine = "\n"
-        let confirmed = confirm(age + newLine + gender + newLine + city);
-        if (!confirmed) {
-            fillInfo();
-        }
+    const age = prompt("How old are you?");
+    const gender = prompt("How do you identify?");
+    const city = prompt("What city do you live in?");
+    const newLine = "\n";
+
+    const confirmed = confirm(age + newLine + gender + newLine + city);
+    if (!confirmed) {
+        fillInfo()
     }
+}
 
-    fillInfo()
+fillInfo();
 
-})();
+
+// function fillInfo() {
+//     let age = prompt("How old are you?");
+//     let gender = prompt("How do you identify?");
+//     let city = prompt("Where do you live?");
+//     let newLine = "\n"
+//     let confirmed = confirm(age + newLine + gender + newLine + city);
+//     if (!confirmed) {
+//         fillInfo();
+//     }
+// }
+//
+// fillInfo()
