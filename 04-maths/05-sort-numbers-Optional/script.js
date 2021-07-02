1d4be2ed6d2ae8a42c80f36adc2299199828b615
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-02 15:51:35 +0200

    Solve 04e05

--- 04-maths/05-sort-numbers-Optional/script.js
+++ 04-maths/05-sort-numbers-Optional/script.js
@@ -9,14 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to get the value of an input: document.getElementById("element-id").value
+document.getElementById("run").addEventListener("click", function () {
+    let input = document.getElementById("numbers").value
+    let numbers = input.split(", ");
 
-    document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
-
+    numbers.sort(function (a, b) {
+        return a - b;
     });
 
-})();
+    document.getElementById("numbers").value = numbers;
+})
+
+
+
