ccdb3cc8fde666e7aca376bfb4c622e5ce935426
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-05 11:46:07 +0200

    Add notes for 05e11

--- 05-arrays/11-rand-array-stats-Optional/script.js
+++ 05-arrays/11-rand-array-stats-Optional/script.js
@@ -9,24 +9,14 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function () {
 
-    function getRandom(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-    document.getElementById("run").addEventListener("click", function () {
-        document.getElementById("n-1").value = getRandom(100);
-        document.getElementById("n-2").value = getRandom(100);
-        document.getElementById("n-3").value = getRandom(100);
-        document.getElementById("n-4").value = getRandom(100);
-        document.getElementById("n-5").value = getRandom(100);
-        document.getElementById("n-6").value = getRandom(100);
-        document.getElementById("n-7").value = getRandom(100);
-        document.getElementById("n-8").value = getRandom(100);
-        document.getElementById("n-9").value = getRandom(100);
-        document.getElementById("n-10").value = getRandom(100);
-    })
+// function getRandom(max) {
+//     return Math.floor(Math.random() * max);
+// }
+//
+// document.getElementById("run").addEventListener("click", function () {
+//
+// })
 
 
-})();
+
