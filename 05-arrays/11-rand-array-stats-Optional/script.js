eb1ce338f6bd3c39767e7b38fe7cb47118ba3951
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-05 15:43:19 +0200

    Show random numbers in 05e11

--- 05-arrays/11-rand-array-stats-Optional/script.js
+++ 05-arrays/11-rand-array-stats-Optional/script.js
@@ -17,40 +17,22 @@ function getRandom(max) {
 document.getElementById("run").addEventListener("click", function () {
     allRandoms = new Array();
 
-    const random1 = getRandom(100);
-    document.getElementById("n-1").innerHTML = random1;
-    allRandoms.push('random1');
-    const random2 = getRandom(100);
-    document.getElementById("n-2").innerHTML = random2;
-    allRandoms.push('random2');
-    const random3 = getRandom(100);
-    document.getElementById("n-3").innerHTML = random3;
-    allRandoms.push('random3');
-    const random4 = getRandom(100);
-    document.getElementById("n-4").innerHTML = random4;
-    allRandoms.push(parseInt('random4'));
-    const random5 = getRandom(100);
-    document.getElementById("n-5").innerHTML = random5;
-    allRandoms.push('random5');
-    const random6 = getRandom(100);
-    document.getElementById("n-6").innerHTML = random6;
-    allRandoms.push('random6');
-    const random7 = getRandom(100);
-    document.getElementById("n-7").innerHTML = random7;
-    allRandoms.push('random7');
-    const random8 = getRandom(100);
-    document.getElementById("n-8").innerHTML = random8;
-    allRandoms.push('random8');
-    const random9 = getRandom(100);
-    document.getElementById("n-9").innerHTML = random9;
-    allRandoms.push('random9');
-    const random10 = getRandom(100);
-    document.getElementById("n-10").innerHTML = random10;
-    allRandoms.push('random10');
+    document.getElementById('n-1').innerHTML = getRandom(100);
+    document.getElementById('n-2').innerHTML = getRandom(100);
+    document.getElementById('n-3').innerHTML = getRandom(100);
+    document.getElementById('n-4').innerHTML = getRandom(100);
+    document.getElementById('n-5').innerHTML = getRandom(100);
+    document.getElementById('n-6').innerHTML = getRandom(100);
+    document.getElementById('n-7').innerHTML = getRandom(100);
+    document.getElementById('n-8').innerHTML = getRandom(100);
+    document.getElementById('n-9').innerHTML = getRandom(100);
+    document.getElementById('n-10').innerHTML = getRandom(100);
 
-document.getElementById('min').innerHTML = Math.min('allRandoms');
-
-
+    //TODO: push all random numbers to an array
+    //get a minimum
+    //get a maxiumum
+    //get the sum
+    //get the average
 
 })
 
