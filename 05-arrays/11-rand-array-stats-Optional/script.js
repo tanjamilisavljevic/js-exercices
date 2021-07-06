62f99604ee8b514288f22e8821b0d9efa57a5560
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-06 14:08:30 +0200

    Refactor functions into a loop

--- 05-arrays/11-rand-array-stats-Optional/script.js
+++ 05-arrays/11-rand-array-stats-Optional/script.js
@@ -9,45 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-
-function getRandom(max, min) {
-    return Math.floor(Math.random() * (max - min) + min);
+const getRandom = (max, min) => Math.floor(Math.random() * (max - min) + min);
+const displayAllRandomsInTargets = (randomNumber, target) => {
+    document.getElementById(target).innerHTML = randomNumber;
 }
-
-
 document.getElementById("run").addEventListener("click", function () {
-    allRandoms = new Array();
+    const allRandoms = [];
 
-    const n1 = getRandom(100, 1);
-    allRandoms.push(n1.valueOf());
-    document.getElementById('n-1').innerHTML = n1;
-    const n2 = getRandom(100, 1);
-    allRandoms.push(n2);
-    document.getElementById('n-2').innerHTML = n2;
-    const n3 = getRandom(100, 1);
-    allRandoms.push(n3);
-    document.getElementById('n-3').innerHTML = n3;
-    const n4 = getRandom(100, 1);
-    allRandoms.push(n4);
-    document.getElementById('n-4').innerHTML = n4;
-    const n5 = getRandom(100, 1);
-    allRandoms.push(n5);
-    document.getElementById('n-5').innerHTML = n5;
-    const n6 = getRandom(100, 1);
-    allRandoms.push(n6);
-    document.getElementById('n-6').innerHTML = n6;
-    const n7 = getRandom(100, 1);
-    allRandoms.push(n7);
-    document.getElementById('n-7').innerHTML = n7;
-    const n8 = getRandom(100, 1);
-    allRandoms.push(n8);
-    document.getElementById('n-8').innerHTML = n8;
-    const n9 = getRandom(100, 1);
-    allRandoms.push(n9);
-    document.getElementById('n-9').innerHTML = n9;
-    const n10 = getRandom(100, 1);
-    allRandoms.push(n10);
-    document.getElementById('n-10').innerHTML = n10;
+    for (let i = 1; i <= 10; i++) {
+        const randomNumber = getRandom(100, 1)
+        displayAllRandomsInTargets(randomNumber, 'n-' + i);
+        allRandoms.push(randomNumber)
+    }
 
     document.getElementById('min').innerHTML = Math.min(...allRandoms);
     document.getElementById('max').innerHTML = Math.max(...allRandoms);
