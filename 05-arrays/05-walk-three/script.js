45bb4c43490cdf9cd7674508083f4b9bdc72c5d9
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-06 15:32:30 +0200

    Rewrite 05e05

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -10,43 +10,52 @@
 // You will have time to focus on it later.
 
 
-    const people = [
-        {
-            firstname: "Garald",
-            lastname: "Freschi",
-            email: "gfreschi0@discuz.net",
-        },
-        {
-            firstname: "Bendicty",
-            lastname: "Foster-Smith",
-            email: "bfostersmith1@mysql.com",
-        },
-        {
-            firstname: "Michelle",
-            lastname: "Draude",
-            email: "mdraude2@csmonitor.com",
-        },
-        {
-            firstname: "Cathee",
-            lastname: "Meineck",
-            email: "cmeineck3@t-online.de",
-        },
-        {
-            firstname: "Marianna",
-            lastname: "Coal",
-            email: "mcoal4@creativecommons.org",
-        },
-    ];
+const people = [
+    {
+        firstname: "Garald",
+        lastname: "Freschi",
+        email: "gfreschi0@discuz.net",
+    },
+    {
+        firstname: "Bendicty",
+        lastname: "Foster-Smith",
+        email: "bfostersmith1@mysql.com",
+    },
+    {
+        firstname: "Michelle",
+        lastname: "Draude",
+        email: "mdraude2@csmonitor.com",
+    },
+    {
+        firstname: "Cathee",
+        lastname: "Meineck",
+        email: "cmeineck3@t-online.de",
+    },
+    {
+        firstname: "Marianna",
+        lastname: "Coal",
+        email: "mcoal4@creativecommons.org",
+    },
+];
 
-    // your code here
-    document.getElementById('run').onclick = function () {
 
-        people.forEach(function (person) {
-            const name = person.firstname + ' ' + person.lastname;
-            console.log(name)
-        })
+document.getElementById("run").onclick = () => {
 
-    }
+    people.forEach(function (person) {
+        console.log(person.firstname + " " + person.lastname)
+    })
+
+}
+
+// your code here
+// document.getElementById('run').onclick = function () {
+//
+//     people.forEach(function (person) {
+//         const name = person.firstname + ' ' + person.lastname;
+//         console.log(name)
+//     })
+//
+// }
 
 
 
